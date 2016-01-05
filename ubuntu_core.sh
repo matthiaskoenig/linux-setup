14e197fb5f0edebd30ab77b774fd127559402d4a
Matthias Koenig <konigmatt@googlemail.com>
2016-01-05 12:16:59 +0100

    basic ubuntu setup

---
+++ ubuntu_core.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+############################################################
+# Core Ubuntu setup after installation
+#
+# @author: Matthias Koenig
+# @date: 2016-01-05
+#
+# Issues
+# - how to sudo with password?
+############################################################
+
+# Update the system
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install the scripts
+# sudo apt-get install git
+# GIT_DIR=$HOME/git
+# mkdir $GIT_DIR
+# cd $GIT_DIR
+# git clone https://github.com/matthiaskoenig/linux-setup
