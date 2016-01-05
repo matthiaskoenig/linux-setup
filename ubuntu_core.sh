d1541acc01ab8ba11a9463806baeb7a73b692ec4
Matthias Koenig <konigmatt@googlemail.com>
2016-01-05 18:01:46 +0100

    old changes ubuntu core

--- ubuntu_core.sh
+++ ubuntu_core.sh
@@ -9,13 +9,17 @@
 # - how to sudo with password?
 ############################################################
 
-# Update the system
-sudo apt-get update
-sudo apt-get upgrade
-
 # Install the scripts
 # sudo apt-get install git
 # GIT_DIR=$HOME/git
 # mkdir $GIT_DIR
 # cd $GIT_DIR
 # git clone https://github.com/matthiaskoenig/linux-setup
+
+
+
+# Update the system
+sudo apt-get update
+sudo apt-get upgrade
+
+
