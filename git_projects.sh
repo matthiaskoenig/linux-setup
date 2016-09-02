52d6e7d140791a6ae40094864c1efdf5fcdcf393
Matthias Koenig <konigmatt@googlemail.com>
2016-09-02 13:55:36 +0200

    cytoscape and robundle projects added

---
+++ git_projects.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+############################################################
+# Clone the git projects
+############################################################
+GITDIR=$HOME/git
+mkdir ${GITDIR}
+cd ${GITDIR}
+
+# Cytoscape
+git clone https://github.com/matthiaskoenig/cy3sbml
+git clone https://github.com/matthiaskoenig/cy3robundle
+git clone https://github.com/matthiaskoenig/cy3javascript
+
+git clone https://github.com/cytoscape/cytoscape-impl.git
+cd cytoscape-impl
+git checkout 3.4.0
+cd ..
+
+git clone https://github.com/cytoscape/cytoscape-api.git
+cd cytoscape-api
+git checkout 3.4.0
+cd ..
+
+git clone https://github.com/apache/incubator-taverna-language.git
