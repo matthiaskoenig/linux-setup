5f2e2b948901956d12c4da9c49f48774618db89c
Matthias Koenig <konigmatt@googlemail.com>
2016-09-07 11:46:12 +0200

    additional repos

--- git_projects.sh
+++ git_projects.sh
@@ -22,3 +22,10 @@ git checkout 3.4.0
 cd ..
 
 git clone https://github.com/apache/incubator-taverna-language.git
+git clone https://github.com/PRIDE-Utilities/ols-client.git
+
+# PLF
+git clone https://github.com/matthiaskoenig/sbmlutils
+git clone https://github.com/matthiaskoenig/modelmanager/
+git clone https://github.com/matthiaskoenig/multiscale-galactose
+
