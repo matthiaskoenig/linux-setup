b98c62e56ecdf6094a10b76e2f12ccb191f7006d
Matthias Koenig <konigmatt@googlemail.com>
2016-09-16 16:31:31 +0200

    first version of trying make

--- libCombine.sh
+++ libCombine.sh
@@ -6,24 +6,24 @@
 # Usage: 
 # 	./libCombine.sh 2>&1 | tee ./logs/libCombine.log
 #
+# Depens on Zipper library
 ############################################################
 date
 echo "--------------------------------------"
 echo "libCombine installation"
 echo "--------------------------------------"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-COMBINECODE=libCombine
-
+CODE=libCombine
+REPO=https://github.com/sbmlteam/libCombine.git
 
 GIT_DIR=$HOME/git
 TMP_DIR=$HOME/tmp
-if ! [ -d "$SVN_DIR" ]; then
-	mkdir $SVN_DIR
+if ! [ -d "${GIT_DIR}" ]; then
+	mkdir ${GIT_DIR}
 fi
-if ! [ -d "$TMP_DIR" ]; then
-	mkdir $TMP_DIR
+if ! [ -d "${TMP_DIR}" ]; then
+	mkdir ${TMP_DIR}
 fi
-exit
 
 echo "---------------------------------------"
 echo "install libCombine dependencies"
@@ -31,44 +31,57 @@ echo "---------------------------------------"
 sudo apt-get -y install cmake cmake-gui swig libxml2 libxml2-dev libbz2-dev zlib1g-dev
 
 echo "--------------------------------------"
-echo "pull libNUML repository"
+echo "pull libCombine repository"
 echo "--------------------------------------"
-echo "$GIT_DIR/${COMBINECODE}"
-if [ -d "$GIT_DIR/${COMBINECODE}" ]; then
-	cd $GIT_DIR/$COMBINECODE
+echo "$GIT_DIR/${CODE}"
+if [ -d "$GIT_DIR/${CODE}" ]; then
+	cd $GIT_DIR/${CODE}
 	git pull
-    git checkout 46558fcea3d
+    git checkout master
 else
 	cd $GIT_DIR
-	git clone https://github.com/NuML/NuML.git $NUMLCODE
-	cd $GIT_DIR/$NUMLCODE
+	git clone ${REPO} ${CODE}
+	cd $GIT_DIR/${CODE}
 fi
 echo "*commit*"
 git rev-parse HEAD
-
 echo 
 
 echo "--------------------------------------"
-echo "build libNUML"
+echo "build libCombine"
 echo "--------------------------------------"
-NUML_BUILD=$TMP_DIR/numl_build
-if [ -d "$NUML_BUILD" ]; then
-	sudo rm -rf $NUML_BUILD
+BUILD=$TMP_DIR/libCombine_build
+if [ -d "${BUILD}" ]; then
+	sudo rm -rf ${BUILD}
 fi
-mkdir $NUML_BUILD
+mkdir ${BUILD}
 
 # here are the cmake files
-cd $NUML_BUILD
+cd ${BUILD}
 
-cmake -DEXTRA_LIBS="xml2;z;bz2;" -DWITH_JAVA=ON -DWITH_PYTHON=ON -DWITH_R=ON ${GIT_DIR}/$NUMLCODE/libnuml
+cmake -DEXTRA_LIBS="xml2;z;bz2;" -DWITH_JAVA=ON -DWITH_PYTHON=ON -DWITH_R=ON ${GIT_DIR}/$CODE
 make -j8
 
+exit
+
 echo "--------------------------------------"
-echo "install libnuml"
+echo "install libCombine"
 echo "--------------------------------------"
 # remove old files
-sudo rm -rf /usr/local/share/libnuml
-sudo rm -rf /usr/local/include/numl/
-sudo rm -rf /usr/local/lib/libnuml*
+# TODO
+# sudo rm -rf /usr/local/share/libsedml
+# sudo rm -rf /usr/local/include/sedml
+# sudo rm -rf /usr/local/lib/libsedml*
+# sudo rm /usr/local/share/java/libsedmlj.jar
+# sudo rm /usr/local/lib/python2.7/site-packages/libsedml/_libsedml.so
+# sudo rm /usr/local/lib/python2.7/site-packages/libsedml.pth
+# sudo rm /usr/local/lib/python2.7/site-packages/libsedml/libsedml.py
+# sudo rm /etc/profile.d/libnuml.sh
+# sudo rm /etc/profile.d/libsedml.sh
+
 # installation
 sudo make install
+
+echo "--------------------------------------"
+echo "python bindings"
+echo "--------------------------------------"
