e4e46829b07f2949ce427026f57ed2d3ad98c281
Matthias Koenig <konigmatt@googlemail.com>
2016-10-06 18:34:36 +0200

    working on base image

--- make_images.sh
+++ make_images.sh
@@ -12,19 +12,18 @@
 # see also: https://github.com/jupyter/docker-stacks/blob/master/Makefile
 ##########################################################
 
-VERSION=v0.1.1a
 OWNER=matthiaskoenig
 GIT_MASTER_HEAD_SHA=$(git rev-parse --short=12 --verify HEAD)
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-# make `linux-setup-base`
+# build latest `linux-setup-base` image
 DOCKER_BASE=linux-setup-base
-docker build -t ${DOCKER_BASE}:${VERSION} ./${DOCKER_BASE}
+docker build -t ${DOCKER_BASE} ./${DOCKER_BASE}
 
 ##tag the latest stack image with the HEAD git SHA
-docker tag -f ${OWNER}/$():latest $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
+docker tag -f ${DOCKER_BASE}:latest ${DOCKER_BASE}:${GIT_MASTER_HEAD_SHA}
 
 # TODO: test
 
@@ -33,8 +32,7 @@ docker tag -f ${OWNER}/$():latest $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
 # Use commit for tagging
 
 
-
-docker push $(OWNER)/$(notdir $@):latest
-docker push $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
+# docker push $(OWNER)/$(notdir $@):latest
+# docker push $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
 
 # 032014150ae6
