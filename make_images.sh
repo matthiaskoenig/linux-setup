a72617c1c579dac9565d51126aba1c55f12b1c72
Matthias Koenig <konigmatt@googlemail.com>
2016-10-06 16:50:04 +0200

    working on dockerfile

--- make_images.sh
+++ make_images.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
+##########################################################
+# Builds all the docker images.
+# The dependency tree of the images is
+#
+# linux-setup-base
+#  |
+#   --> linux-setup-combine
+#        |
+#         --> linux-setup-notebook
+#
+# see also: https://github.com/jupyter/docker-stacks/blob/master/Makefile
+##########################################################
+
 VERSION=v0.1.1a
+OWNER=matthiaskoenig
+GIT_MASTER_HEAD_SHA=$(git rev-parse --short=12 --verify HEAD)
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
@@ -8,5 +23,18 @@ cd ${DIR}
 DOCKER_BASE=linux-setup-base
 docker build -t ${DOCKER_BASE}:${VERSION} ./${DOCKER_BASE}
 
-# push to docker hub
+##tag the latest stack image with the HEAD git SHA
+docker tag -f ${OWNER}/$():latest $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
 
+# TODO: test
+
+# TODO: push to docker hub
+
+# Use commit for tagging
+
+
+
+docker push $(OWNER)/$(notdir $@):latest
+docker push $(OWNER)/$(notdir $@):$(GIT_MASTER_HEAD_SHA)
+
+# 032014150ae6
