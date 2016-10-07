0d1c2c65be4ffb476dada06cfce97673278df045
Matthias Koenig <konigmatt@googlemail.com>
2016-10-07 19:17:32 +0200

    working on user setup for spyder

--- run_spyder.sh
+++ run_spyder.sh
@@ -6,18 +6,21 @@
 #################################################
 
 # TODO: set user variable
+# export USER=user
 
 # TODO: connect GUI
 # root@core1:~# /usr/local/bin/spyder
 # No protocol specified
 # Spyder: cannot connect to X server unix:0.0
 
+# http://stackoverflow.com/questions/25281992/alternatives-to-ssh-x11-forwarding-for-docker-containers/25334301#25334301
 
 docker run -it \
 --net host \
 --cpuset-cpus 0 \
 --memory 512mb \
--v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -e DISPLAY=unix$DISPLAY \
-matthiaskoenig/linux-setup-combine /bin/bash # /usr/local/bin/spyder
+-u combine \
+matthiaskoenig/linux-setup-combine /bin/bash /usr/local/bin/spyder
 # --name spyder \
