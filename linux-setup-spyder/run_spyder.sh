a483057dbdeea0c235001dffaea35c9fce3f6719
Matthias Koenig <konigmatt@googlemail.com>
2016-10-07 20:21:22 +0200

    working on spyder in docker container

--- linux-setup-spyder/run_spyder.sh
+++ linux-setup-spyder/run_spyder.sh
@@ -5,15 +5,20 @@
 # https://blog.jessfraz.com/post/docker-containers-on-the-desktop/
 #################################################
 
-# TODO: set user variable
-# export USER=user
-
 # TODO: connect GUI
 # root@core1:~# /usr/local/bin/spyder
 # No protocol specified
 # Spyder: cannot connect to X server unix:0.0
 
+# !!! don't do this
+xhost +
+
+# Granting access only to a specific trusted docker container by using the containers hostname.
+# xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
+
+
 # http://stackoverflow.com/questions/25281992/alternatives-to-ssh-x11-forwarding-for-docker-containers/25334301#25334301
+# http://olivier.barais.fr/blog/posts/2014.08.26/Eclipse_in_docker_container.html
 
 docker run -it \
 --net host \
@@ -21,6 +26,7 @@ docker run -it \
 --memory 512mb \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -e DISPLAY=unix$DISPLAY \
--u combine \
-matthiaskoenig/linux-setup-combine /bin/bash /usr/local/bin/spyder
-# --name spyder \
+-e USER=combine \
+matthiaskoenig/linux-setup-combine spyder
+
+xhost -
