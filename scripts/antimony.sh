e2b68954d1c911a27abbbc1334db008d394128a4
Matthias Koenig <konigmatt@googlemail.com>
2016-10-18 20:49:18 +0200

    working on antimony with cellml

--- scripts/antimony.sh
+++ scripts/antimony.sh
@@ -47,6 +47,7 @@ else
 	svn checkout http://svn.code.sf.net/p/antimony/code/ $CODE
 	cd ${SVN_DIR}/$CODE
 fi
+# svn update -r3617
 
 echo "--------------------------------------"
 echo "build antimony"
@@ -63,15 +64,36 @@ mkdir $ANTIMONY_BUILD
 # CMAKE_CXX_FLAGS = -fPIC
 cd $ANTIMONY_BUILD
 # with cellml
-cmake -DWITH_CELLML=ON -DCELLML_API_INSTALL_DIR="/usr/local/lib/cellml-sdk-1.12-Linux-x86_64" -DWITH_PYTHON=ON ${SVN_DIR}/$CODE/antimony
+# cmake -DWITH_CELLML=ON -DCELLML_API_INSTALL_DIR="/usr/local/lib/cellml-sdk-1.13-Linux-x86_64" -DWITH_PYTHON=ON ${SVN_DIR}/$CODE/antimony
 # without cellml
-# cmake -DWITH_CELLML=OFF -DWITH_PYTHON=ON ${SVN_DIR}/$CODE/antimony
-make -j8
+cmake -DWITH_CELLML=OFF -DWITH_PYTHON=ON ${SVN_DIR}/$CODE/antimony
+make # DONT ! -j8
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 echo "--------------------------------------"
 echo "install antimony"
 echo "--------------------------------------"
+
+# sudo rm /usr/local/lib/libantimony.so
+# sudo rm /usr/local/lib/libantimony-static.a
+# sudo rm /usr/local/bin/rehashantimony
+# sudo rm /usr/local/bin/testantimony
+# sudo rm /usr/local/bin/sbtranslate
+# sudo rm /usr/local/bin/antimony2sbml
+# sudo rm /usr/local/bin/sbml2antimony
+# sudo rm /usr/local/bin/antimony2cellml
+# sudo rm /usr/local/bin/cellml2antimony
+# sudo rm /usr/local/bin/QTAntimony
+# sudo rm /usr/local/bin/antimony.ico
+
+# sudo rm -rf /usr/local/lib/python2.7/dist-packages/antimony
+
+
 sudo make -j8 install
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+
+
 
 echo "--------------------------------------"
 echo "test antimony"
--- scripts/libcellml.sh
+++ scripts/libcellml.sh
@@ -2,12 +2,15 @@
 ############################################################
 # Make cellml libraries available
 #   http://cellml-api.sourceforge.net/download.html
+#   https://www.cellml.org/tools/downloads/cellml_api/releases/1.11
 #
 # Usage: 
 # 	./libcellml.sh 2>&1 | tee ./logs/libcellml.log
 ############################################################
 
-VERSION=cellml-sdk-1.12-Linux-x86_64
+#VERSION=cellml-sdk-1.11-Linux-x86_64
+VERSION=cellml-sdk-1.13-Linux-x86_64
+
 
 TMP_DIR=$HOME/tmp
 if ! [ -d "$TMP_DIR" ]; then
@@ -16,7 +19,9 @@ fi
 
 cd ${TMP_DIR}
 
-wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.12/20121031/linux-x86_64/${VERSION}.tar.bz2/download
+# wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.12/20121031/linux-x86_64/${VERSION}.tar.bz2/download
+# wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.11/20120418/linux-x86_64/${VERSION}.tar.bz2/download
+wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.13/20130716/linux-x86_64/${VERSION}.tar.bz2/download
 mv download ${VERSION}.tar.bz2
 tar jxfv ${VERSION}.tar.bz2
 rm ${VERSION}.tar.bz2
