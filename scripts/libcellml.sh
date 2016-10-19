0215793ceeaab653f63319a529dbf5b40dc46950
Matthias Koenig <konigmatt@googlemail.com>
2016-10-19 09:53:30 +0200

    trying to build the SDK

--- scripts/libcellml.sh
+++ scripts/libcellml.sh
@@ -9,15 +9,18 @@
 ############################################################
 
 #VERSION=cellml-sdk-1.11-Linux-x86_64
+# VERSION=cellml-sdk-1.12-Linux-x86_64
 VERSION=cellml-sdk-1.13-Linux-x86_64
-
 
 TMP_DIR=$HOME/tmp
 if ! [ -d "$TMP_DIR" ]; then
 	mkdir $TMP_DIR
 fi
-
 cd ${TMP_DIR}
+
+##############################
+# Install prebuild library
+##############################
 
 # wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.12/20121031/linux-x86_64/${VERSION}.tar.bz2/download
 # wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.11/20120418/linux-x86_64/${VERSION}.tar.bz2/download
@@ -26,7 +29,7 @@ mv download ${VERSION}.tar.bz2
 tar jxfv ${VERSION}.tar.bz2
 rm ${VERSION}.tar.bz2
 
-sudo mv ${TMP_DIR}/${VERSION} /usr/local/lib/
+sudo cp ${TMP_DIR}/${VERSION} /usr/local/lib/
 
 echo "Adding to LD_LIBRARY_PATH: /usr/local/lib/${VERSION}/lib"
 cat > libcellml.sh << EOF0
@@ -36,3 +39,27 @@ EOF0
 sudo mv libcellml.sh /etc/profile.d/
 source /etc/profile.d/libcellml.sh
 
+
+##############################
+# Build cellml
+##############################
+
+#sudo apt-get install omniidl
+#
+#cd ${TMP_DIR}
+#API_VERSION=cellml-api-1.13
+#wget https://sourceforge.net/projects/cellml-api/files/CellML-API-Nightly/1.13/20130716/src/${API_VERSION}.tar.bz2/download
+#mv download ${API_VERSION}.tar.bz2
+#tar jxfv ${API_VERSION}.tar.bz2
+#
+#CELLML_BUILD=$TMP_DIR/cellml_build
+#if ! [ -d "$CELLML_BUILD" ]; then
+#	mkdir $CELLML_BUILD
+#fi
+#cd ${CELLML_BUILD}
+#${TMP_DIR}/${VERSION}
+#cmake -DCMAKE_INCLUDE_DIRECTORIES=${TMP_DIR}/${VERSION}/include -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${CELLML_BUILD} ../${API_VERSION}/ 2>&1 | tee cellml.log
+#
+#make all
+#make test
+#make install
