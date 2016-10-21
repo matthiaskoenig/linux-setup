c8a4f6c8a60fae95b847a6bde6c30876bb3a8b75
Matthias Koenig <konigmatt@googlemail.com>
2016-10-21 08:58:47 +0200

    checking return types of sub-scripts to abort build in case of error

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -19,19 +19,37 @@ if ! [ -d "${LOGS_DIR}" ]; then
 fi
 
 ./libsbml.sh 2>&1 | tee ${LOGS_DIR}/libsbml.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./libsedml.sh 2>&1 | tee ${LOGS_DIR}/libsedml.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./cellml-api.sh 2>&1 | tee ${LOGS_DIR}/cellml-api.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./roadrunner-deps.sh 2>&1 | tee ${LOGS_DIR}/roadrunner-deps.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./roadrunner.sh 2>&1 | tee ${LOGS_DIR}/roadrunner.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./antimony.sh 2>&1 | tee ${LOGS_DIR}/antimony.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./phrasedml.sh 2>&1 | tee ${LOGS_DIR}/phrasedml.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./zipper.sh 2>&1 | tee ${LOGS_DIR}/zipper.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./libcombine.sh 2>&1 | tee ${LOGS_DIR}/libcombine.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./sbml2matlab.sh 2>&1 | tee ${LOGS_DIR}/sbml2matlab.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 ./spyder.sh 2>&1 | tee ${LOGS_DIR}/spyder.log
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # ./tellurium.sh 2>&1 | tee ${LOGS_DIR}/tellurium.log
 
