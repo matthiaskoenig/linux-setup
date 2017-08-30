c1b89877f9d2812e2639148ad3a37465480106da
Matthias Koenig <konigmatt@googlemail.com>
2017-08-30 14:37:53 +0200

    fixed pip issue in install script

--- scripts/ubuntu_core.sh
+++ scripts/ubuntu_core.sh
@@ -6,6 +6,7 @@
 # used as workstation
 #
 # Usage: 
+#   mkdir logs
 # 	./ubuntu_core.sh 2>&1 | tee ./logs/ubuntu_core.log
 #
 # Subsequent installation:
@@ -31,7 +32,6 @@ mkdir $HOME/svn
 echo "*** Update & upgrade system ***"
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo pip install --upgrade setuptools pip
 
 echo "*** Install codecs ***"
 sudo apt-get -y install ubuntu-restricted-extras libxine1-ffmpeg gxine mencoder mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6v5 icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 libjpeg-progs flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview flac mpeg3-utils mpegdemux liba52-0.7.4-dev 
@@ -61,7 +61,10 @@ sudo apt-get -y install maven
 echo "*** R ***"
 sudo -E add-apt-repository -y ppa:marutter/rrutter
 sudo -E apt-get update
-sudo -E apt-get -y install r-base-core r-base r-base-dev r-recommended libcurl4-gnutls-dev libxml2-dev libssl-dev python-pip liblzma-dev lzma lzma-dev
+sudo -E apt-get -y install r-base-core r-base r-base-dev r-recommended libcurl4-gnutls-dev libxml2-dev libssl-dev liblzma-dev lzma lzma-dev
+
+echo "*** htop ***"
+sudo apt-get install htop
 
 echo "*** Java ***"
 # remove openjdk
@@ -94,7 +97,8 @@ echo "-----------------------------------------"
 echo "Python dependencies"
 echo "-----------------------------------------"
 sudo apt-get -y install build-essential python-dev python-pip libfreetype6-dev
-sudo apt-get -y install python-tk python3-tk
+sudo apt-get -y install python-tk python3-tk python-pip
+sudo pip install --upgrade setuptools pip
 sudo -E pip install numpy scipy matplotlib ipython pandas jupyter --upgrade
 sudo -E pip install virtualenv virtualenvwrapper --upgrade
 
